3a3ad8593fa04337de1a1be2a40e56e17ccb9517
Otee <oitee.codes@gmail.com>
2021-12-31 03:05:18 +0530

    Add functionality to the insert route

--- src/search.js
+++ src/search.js
@@ -1,5 +1,45 @@
-export function insertDoc(request, response) {
-  return response.send("Insert  route");
+export async function insertDoc(request, response) {
+  //this is a post route
+  // order of inserting documents:
+  /**
+    {document:
+        [{
+            id: 123,
+            doc: "abc"
+        }, 
+        {
+            id: 123,
+            doc: "abc"
+        }]
+
+    } 
+
+     */
+  const resBody = await request.body;
+  if (
+    typeof resBody !== "object" ||
+    Object.keys(resBody).length === 0 ||
+    !resBody.document ||
+    !Array.isArray(resBody.document)
+  ) {
+    console.log(resBody);
+    return response.status(400).json({ error: "Response Body invalid" });
+  }
+  const store = request.store;
+  let insertedDocCount = 0;
+  for (let document of resBody.document) {
+    if (document.id && document.doc) {
+      await store.insert(document.id, document.doc);
+      insertedDocCount++;
+    }
+  }
+
+  if (insertedDocCount > 0) {
+    return response.json({
+      message: `Successfully inserted ${insertedDocCount} documents`,
+    });
+  }
+  return response.json({ message: `No documents found.` });
 }
 
 export function searchDoc(request, response) {
--- src/server.js
+++ src/server.js
@@ -2,11 +2,15 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import * as router from "./router.js";
 import * as env from "./env_variables.js";
+import bodyParser from 'body-parser';
 
 const app = express();
-app.use(router.router);
 
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(router.router);
+
+
 
 
 app.listen(env.config().port, () =>
